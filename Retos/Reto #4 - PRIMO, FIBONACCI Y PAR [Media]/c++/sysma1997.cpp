d65e639548ad6b757f1a8c1df52bdfa9aef87ff9
Sebastián Moreno Acero <sysma1997@gmail.com>
2023-10-18 09:35:47 -0500

    Finish challenge #4

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/c++/sysma1997.cpp
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/c++/sysma1997.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 #include <limits>
 
 using namespace std;
 
+bool checkFibonacci(int number);
+
 int main()
 {
-    bool isPrime{false}, isFibonacci{false}, isEven{false};
+    bool isPrime{true}, isFibonacci{false}, isEven{false};
 
     cout << "Digite un numero: ";
     int number{0};
@@ -18,6 +21,42 @@ int main()
         cout << "Digite un numero entero positivo valido: ";
         cin >> number;
     }
+    number = abs(number);
+
+    // check if prime number
+    if (number == 0 || number == 1)
+        isPrime = false;
+    for (int i = 2; i <= number / 2; i++)
+    {
+        if (number % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    // check if fibonacci number
+    if (checkFibonacci(number))
+        isFibonacci = true;
+    // check if even number
+    if (number % 2 == 0)
+        isEven = true;
+
+    cout << number << " "
+         << (isPrime ? "es primo" : "no es primo") << ", "
+         << (isFibonacci ? "fibonacci" : "no es fibonacci") << " "
+         << (isEven ? "y es par" : "y es impar") << '\n';
 
     return 0;
+}
+
+bool checkFibonacci(int number)
+{
+    auto isPerfectSquare = [](int x) -> bool
+    {
+        int square = sqrt(x);
+        return square * square == x;
+    };
+
+    return isPerfectSquare(5 * number * number + 4) ||
+           isPerfectSquare(5 * number * number - 4);
 }
